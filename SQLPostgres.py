a608a34a497896445ddf564aaf98ccd00c47f05e
vlatterran <soboleff@mail.ru>
2023-03-19 16:36:15 +0300

    a lot of refactoring

--- SQLPostgres.py
+++ SQLPostgres.py
@@ -1,33 +1,27 @@
 import psycopg2
+
 import config
 
-conf = config.conf()      
+conf = config.config()
+logger = conf.logger
 # Подключение к базе данных PostgreSQL
-conn = psycopg2.connect(
-    host=conf.host,
-    database=conf.database,
-    user=conf.user,
-    password=conf.password,
-    port=conf.port
-    )
+with (psycopg2.connect(host=conf.host,
+                       database=conf.database,
+                       user=conf.user,
+                       password=conf.password,
+                       port=conf.port
+                       ) as conn,
+      conn.cursor() as cur):
+    # SQL-запрос для создания новой записи
+    sql = "INSERT INTO users (user_id) VALUES (%s);"
 
-# Создание курсора
-cur = conn.cursor()
-
-# SQL-запрос для создания новой записи
-sql = "INSERT INTO users (user_id) VALUES (%s);" 
-
-try:
-    # Выполнение SQL-запроса с передачей параметров
-    cur.execute(sql, (123435,))
-    # Фиксация изменений в базе данных
-    conn.commit()
-    print("Запись успешно добавлена!")
-except Exception as e:
-    # Если возникла ошибка, откатываем транзакцию
-    conn.rollback()
-    print("Ошибка при добавлении записи:", e)
-
-# Закрытие курсора и соединения с базой данных
-cur.close()
-conn.close()
+    try:
+        # Выполнение SQL-запроса с передачей параметров
+        cur.execute(sql, (123435,))
+        # Фиксация изменений в базе данных
+        conn.commit()
+        logger.info("Запись успешно добавлена!")
+    except Exception as e:
+        # Если возникла ошибка, откатываем транзакцию
+        conn.rollback()
+        logger.exception("Ошибка при добавлении записи")
