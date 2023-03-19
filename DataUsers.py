7ee324a44e45832d8e9a04c8c95a4385be0be4c7
vlatterran <soboleff@mail.ru>
2023-03-19 16:36:15 +0300

    a lot of refactoring

--- DataUsers.py
+++ DataUsers.py
@@ -1,140 +1,145 @@
+import dataclasses
+from typing import Iterable, Sized
+
 import openai
 
 import config
 
-conf = config.conf()                                        # тут есть переменные из config.json
+conf = config.config()
+
+
 def error_print(e):
-    if(conf.print_DataUsers_errors):
-                print("Произошла ошибка: ", e)
-class ai:
+    if conf.print_DataUsers_errors:
+        conf.logger.exception(e)
+
+
+@dataclasses.dataclass(frozen=True)
+class Message(dict):
+    role: str
+    content: str
+
+    def __len__(self):
+        return len(self.content)
+
+
+class History(Iterable, Sized):
+    def __init__(self):
+        self._data: list[Message] = []
+        self.opened = False
+        self.new = 0
+
+    def trim(self, percent: float = 20):
+        """
+        Удаляет около 20% символов из начала истории сообщений,
+         если по одному их удалять то ответ сети прерываться будет
+        """
+        length = self.size
+
+        aim_length = length * (1 - percent / 100)
+
+        while self.size > aim_length:
+            self._data.pop(0)
+
+    @property
+    def size(self):
+        return sum(map(len, self._data))
+
+    def clear(self):
+        self._data.clear()
+
+    def append(self, message: Message):
+        if self.opened:
+            self._data.append(message)
+            self.new += 1
+        else:
+            raise RuntimeError('Can append messages only in in context manager mode')
+
+    def extend(self, messages: Iterable[Message]):
+        if self.opened:
+            before = len(self)
+            self._data.extend(messages)
+            self.new = len(self) - before
+        else:
+            raise RuntimeError('Can append messages only in in context manager mode')
+
+    def to_primitive(self):
+        return list(
+            dataclasses.asdict(x) for x in self
+        )
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __enter__(self):
+        self.opened = True
+        self.new = 0
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.opened = False
+        if exc_type is not None:
+            self._data = self._data[0:-self.new]
+
+
+class AI:
     openai.api_key = conf.OpenAi_TOKEN
-        
-    def __init__(self,obj):
-        self.comp = obj  
 
-    def generate_response(self, prompt):                    # принимает историю сообщений для того чтобы сеть понимала контекст 
-                                                            #  она выглядит так
-                                                            #  [{"role": "user", "content": mass},      - сообщение пользователя
-                                                            #   {"role": "assistant", "content": ret}]  - ответ сети
+    def __init__(self, obj):
+        self.comp = obj
 
-                                                            #
-                                                            #
-        try:                                                # создаеться запрос, в нем указывается подель и история сообщений 
-            completion = self.comp.create(
-            model="gpt-3.5-turbo", messages= prompt )
-       
-            return completion.choices[0].message.content    # возвращаем ответ сети
-        
-        except openai.error.InvalidRequestError as e:       # эта ошибка возникает если prompt больше 4000 токенов, нужно удалить из него первые сообщения 
-            error_print(e)
-            return 1
-        except ValueError as e:                             # другие ошибки
-            error_print(e)
-            return 2
-        except openai.error.OpenAIError as e:
-            error_print(e)
-            return 3
-        except openai.error.AuthenticationError as e:
-            error_print(e)
-            return 4
-        except Exception as e:
-            error_print(e)
-            return 5
-   
-    
-class user_:                                                # тут есть имя - это айди чата по сути и история сообщений 
+    def generate_response(self, history: History):
+        # принимает историю сообщений для того чтобы сеть понимала контекст
+        #  она выглядит так
+        #  [{"role": "user", "content": mass},      - сообщение пользователя
+        #   {"role": "assistant", "content": ret}]  - ответ сети
 
-    def __init__(self) :
-        self.name = ""
-        self.storeg = []
+        # создаеться запрос, в нем указывается подель и история сообщений
+        completion = self.comp.create(model="gpt-3.5-turbo",
+                                      messages=history.to_primitive())
+
+        return completion.choices[0].message.content  # возвращаем ответ сети
 
 
-class users_:
-    
-    def __init__(self) :
+class User:  # тут есть имя - это id чата по сути и история сообщений
+    def __init__(self, id: int, ai: AI):
+        self.id = id
+        self.history = History()
+        self.ai = ai
+
+    def send_message(self, text: str) -> str:  # создаем запрос
+        message = Message(role="user", content=text)
+        with self.history:
+            self.history.append(message)
+            while True:
+                # передаем историю сообщений в сеть,
+                # если ошибка из-за числа токенов то удаляем часть сообщений и пробуем заново.
+                try:
+                    res = self.ai.generate_response(self.history)
+                    ans = Message(role="assistant",
+                                  content=res)  # если ошибок нет, то добавляем ответ в историю сообщений
+                    self.history.append(ans)
+                    return res
+                except openai.error.InvalidRequestError as e:
+                    self.history.trim()  # удаляем часть сообщений
+                    continue
+
+
+class Users:
+    def __init__(self):
+        self.users: dict[int, User] = {}  # создаем словарь в котором будут пользователи
         try:
-            self.musers = []                                # создаем массив в которм будут пользователи - users_()
-            self.AI = ai(openai.ChatCompletion)
-        except Exception as e:
-            error_print(e)   
-
-    def add_user(self,name):                                # если пользователя с таким именем нет созадаем его.
-        try:
-            i = 0
-            for x in self.musers:
-                if name == x.name:
-                    
-                    return 1
-                i+=1
-            
-            us = user_()
-        
-            us.name = name
-            
-            self.musers.append(us)
-
-            if(conf.print_messages):   
-                        print( 'add user - '+str(name) )
+            self.ai = AI(openai.ChatCompletion)
         except Exception as e:
             error_print(e)
-            
-    def del_storeg(self,name):
-        try:
-            for x in self.musers:
-                if name == x.name:
-                    x.storeg = []
-                    if(conf.print_messages):   
-                        print( '/del '+str(name) )
+            raise
 
-                
-        except Exception as e:
-            error_print(e)
-        
-    def del_n_stor(self, stor):                             # удаляет около 20% символов из начала истории сообщений, если по одному их удалять то ответ сети прерываться будет
-        
-        l = 0
-        lx = 0
-        for x in stor:
-            l+=len(x["content"])
-
-        i = 0
-        for x in stor:
-            i+=1
-            lx+=len(x["content"])
-            if(lx >= l/5) :
-                break        
-        
-        return stor[i:]
-    
-
-    def generate_response_user(self, name, mass):         
-        i = 0
-        for x in self.musers:                               # ищем пользователся по имени 
-                                                            
-            if name == x.name:
-                req = {"role": "user", "content": mass}     # создаем запрос 
-
-                self.musers[i].storeg.append(req)           # добавляем в историю сообщений 
-                
-                while 1:
-                    ret = self.AI.generate_response(self.musers[i].storeg)   # передаем историю сообщений в сеть, если ошибка из за числа токенов то удаляем чать сообщений и пробуем заново.
-                    if ret == 1:
-                        try:
-                            self.musers[i].storeg = self.del_n_stor( self.musers[i].storeg)  # удаляем часть сообщений 
-                        except Exception as e:
-                            error_print(e)
-                            break
-
-                    elif ret == 2 or ret == 3 or ret == 4 or ret == 5  :
-                        return "неизвесная ошибка запроса, повторите попытку"
-                    else:
-                       
-                        
-                        ans = {"role": "assistant", "content": ret}     # если ошибок нет то добавляем ответ в историю сообщений 
-                        self.musers[i].storeg.append(ans)
-                        break
-                
-               
-                return ret  
-            i+=1
-        
+    def get_or_create(self, id: int):  # если пользователя с таким именем нет создаем его
+        if id not in self.users:
+            usr = User(id, self.ai)
+            self.users[id] = usr
+            if conf.print_messages:
+                print(f'add user - ' + str(id))
+        return self.users[id]
--- tg_bot.py
+++ tg_bot.py
@@ -1,95 +1,101 @@
-import time
-import telebot
-from telebot import types
-from DataUsers import users_
-import config
 import threading
 
+import telebot
 
-conf = config.conf()                                            # тут есть переменные из config.json
+import config
+from DataUsers import Users, User
+
+conf = config.config()  # тут есть переменные из config.json
+logger = conf.logger
+# достаем токен для бота тг
+bot = telebot.TeleBot(conf.Tg_TOKEN)
+
+users = Users()
 
 
-API_TOKEN = conf.Tg_TOKEN                                       # достаем токен для бота тг
-
-bot = telebot.TeleBot(API_TOKEN)
-
-Users = users_()
-
-@bot.message_handler(commands=['help', 'start'])                # если в сообщении /start /help выдолняется эта функция 
-def send_welcome(message):                                      
-    if(conf.print_messages):                                    # если сообщения включины выести его 
-        print( '/start '+str(message.chat.id) )
-
-    Users.add_user(name = message.chat.id)                      # проверяет есть ли такой пользователь и добавляет его если нужно
-    
-                                                                # выводим ответ
-    bot.send_message(message.chat.id ,"Привет , можешь просто написать свой вопрос и gpt chat тебе ответит")
-    
-@bot.message_handler(commands=[ 'del'])
+@bot.message_handler(commands=['help', 'start'])  # если в сообщении /start /help выдолняется эта функция
 def send_welcome(message):
-    
-    Users.add_user(name = message.chat.id)
-    Users.del_storeg(name = message.chat.id)
-    
-        
-    bot.send_message(message.chat.id ,"del - ok" )
-   
-                                                                # срабатывает если приишло сообщение не /start /help 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
+    if conf.print_messages:  # если сообщения включины выести его
+        print('/start ' + str(message.chat.id))
 
-    Users.add_user(name = message.chat.id)                      # проверяет есть ли такой пользователь и добавляет его если нужно
-    if(conf.print_messages):                                    # если сообщения включины выести его 
-        print( str(message.chat.id) +' / '+'>> '+message.text)
+    users.get_or_create(id=message.chat.id)  # проверяет есть ли такой пользователь и добавляет его если нужно
 
-    ret = Users.generate_response_user(message.chat.id, message.text)   # этот метод принимает айди чата и сообщения пользователя и позвращает ответ
+    # выводим ответ
+    bot.send_message(
+        message.chat.id,
+        "Привет, можешь просто написать свой вопрос и gpt chat тебе ответит"
+    )
 
-    if(conf.print_messages):                                    # если сообщения включины выести его 
-        print( str(message.chat.id) +' / '+'<< '+ret)
 
-    try:                                                        #  parse_mode = "Markdown" позволяет красиво показывать код написанный сетью, но иногда выдает ошибку
-        bot.send_message(message.chat.id ,str(ret), parse_mode = "Markdown" )
+@bot.message_handler(commands=['del'])
+def send_welcome(message):
+    usr = users.get_or_create(id=message.chat.id)
+    usr.history.clear()
+    bot.send_message(message.chat.id, "del - ok")
+
+
+# срабатывает если пришло сообщение не /start /help
+@bot.message_handler()
+def echo_message(message: telebot.types.Message):
+    usr: User = users.get_or_create(id=message.chat.id)
+    if conf.print_messages:  # если сообщения включены - вывести его
+        logger.info(f'{message.chat.id}\n>> {message.text}')
+
+    try:
+        ret = usr.send_message(message.text)
+    except Exception as e:
+        ret = 'Произошла неизвестная ошибка'
+        logger.exception(e)
+
+    if conf.print_messages:  # если сообщения включены - вывести его
+        logger.info(f'{message.chat.id}\n<< {ret}')
+
+    try:  # parse_mode = "Markdown" позволяет красиво показывать код написанный сетью, но иногда выдает ошибку
+        bot.send_message(message.chat.id, str(ret), parse_mode="Markdown")
     except:
-        bot.send_message(message.chat.id ,str(ret) )
-    
+        bot.send_message(message.chat.id, str(ret))
 
 
 def console():
     while True:
         mess = input(">> ")
-        if len(mess):
-            if mess[0] == "/":
-                if mess == "/get_storeg":
-                    i = 1
-                    while i:
-                        name = str(input("get_storeg >> name = "))
-                        if name == "ex": break
-                        for x in Users.musers:
-                            if name == str(x.name):
-                                i = 0
-                                print("Storeg / ", name , x.storeg)
-                if mess == "/users_len":
-                    
-                    print("users_len = ", len(Users.musers)) 
+        if not len(mess):
+            continue
+        if mess[0] != "/":
+            continue
+        if mess == "/get_storeg":
+            i = 1
+            while i:
+                name = str(input("get_storeg >> name = "))
+                if name == "ex": break
+                for x in users.users:
+                    if name == str(x.name):
+                        i = 0
+                        print("Storeg / ", name, x.history)
+        if mess == "/users_len":
+            print(f'users_len = {len(users.users)}')
+
+        if mess == "/code":
+            while True:
+                command = input("command >> ")
+                if command == "exit":
+                    break
+                try:
+                    eval(command)
+                except Exception as e:
+                    logger.exception(e)
 
 
-                if mess == "/code":
-                    while True:
-                        command = input("command >> ")
-                        if command == "ex": break
-                        try:
-                            eval(command)                   
-                        except Exception as e:
-                            print("<< error: ", e)    
 def console2():
     while True:
         command = input(">> ")
         try:
-            eval(command)                   
+            eval(command)
         except Exception as e:
-            print("<< error: ", e)
+            logger.exception(e)
 
-t = threading.Thread(target= console)                           # консоль для отладки 
-t.start()
 
-bot.infinity_polling()                                          # запуск бота 
+# t = threading.Thread(target=console)  # консоль для отладки
+# t.start()
+
+bot.infinity_polling()  # запуск бота
